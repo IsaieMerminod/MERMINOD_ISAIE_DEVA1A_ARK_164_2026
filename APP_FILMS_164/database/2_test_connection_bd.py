dc0796cd509e45a1758efc79f03c48483f5d6e82
Isaie22 <isaie.merminod@gri.ch>
2026-04-24 14:09:36 +0200

    Mise en place de la structure ARK et des fichiers CRUD

--- APP_FILMS_164/database/2_test_connection_bd.py
+++ APP_FILMS_164/database/2_test_connection_bd.py
@@ -1,6 +1,6 @@
 """Démonstration d'envoi d'une requête SQL à la BD
 Fichier : 2_test_connection_bd.py
-Auteur : OM 2023.03.21
+Auteur : OM 2023.03.21 / Modifié pour le projet ARK
 """
 
 from APP_FILMS_164.database.database_tools import DBconnection
@@ -8,17 +8,21 @@ from APP_FILMS_164.database.database_tools import DBconnection
 try:
     """
         Une seule requête pour montrer la récupération des données de la BD en MySql.
+        On va tester si on arrive à lire tes créatures (même si la table est vide).
     """
-    strsql_genres_afficher = """SELECT id_genre, intitule_genre, date_ins_genre FROM t_genre ORDER BY id_genre ASC"""
+    # ICI : On remplace la requête du prof par la tienne
+    strsql_creatures_afficher = """SELECT * FROM Creatures ORDER BY ID_Creature ASC"""
 
     with DBconnection() as db:
-        db.execute(strsql_genres_afficher)
+        # On exécute ta nouvelle requête
+        db.execute(strsql_creatures_afficher)
         result = db.fetchall()
-        print("data_genres ", result, " Type : ", type(result))
+
+        # On affiche le résultat dans la console en bas de l'écran
+        print("Résultat de la table Creatures : ", result, " Type : ", type(result))
 
 
 except Exception as erreur:
-    # print(f"2547821146 Connection à la BD Impossible ! {type(erreur)} args {erreur.args}")
     print(f"2547821146 Test connection BD !"
           f"{__name__,erreur} , "
           f"{repr(erreur)}, "
